ba1155b0bc39b898ad47a6323b6814e4ffab96c4
Aswin0056 <aswinayyappan056@gmail.com>
2025-04-22 11:01:49 +0530

    updated-app

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/login",
+        "https://studio-bd.onrender.com/api/login",
         {
           email,
           password,
