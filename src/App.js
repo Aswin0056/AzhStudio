88b43bcdc5a55ecec737db27e285d6096847ce96
Aswin0056 <aswinayyappan056@gmail.com>
2025-04-25 16:25:41 +0530

    removed-register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute"; // ✅ Adjust path if needed
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          {/* <Route path="/register" element={<Register />} /> */}
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
             {/* ✅ Protected Route */}
